e11718c48853d17da37408d8aad09708127b65ee
JJGayso <gaysojj@rose-hulman.edu>
2017-04-24 14:34:51 -0400

    More Changes for Docker Testing

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,10 @@
-FROM node:4-onbuild
+FROM node
+
+# install your application's dependencies
+RUN npm install
+
+# replace this with your application's default port
 EXPOSE 3000
+
+# replace this with your main "server" script file
+CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -23,25 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
